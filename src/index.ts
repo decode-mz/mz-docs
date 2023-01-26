832bcb4066eee6a16ead3125429370edf3006eba
nairo-mudumane <nairomudumane@outlook.com>
2023-01-26 09:32:35 +0200

    nuit module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { isValid, format, generate } from "./bi";
+import {} from "./nuit";
 
 /**
- * All BI modules
+ * All BI methods
  */
 const bi = { isValid, format, generate };
+
+/**
+ * All NUIT methods
+ */
+const nuit = {};
 
 export { bi as bi };
---
+++ src/nuit/is-valid.ts
@@ -0,0 +1,8 @@
+/**
+ * check if is valid NUIT number
+ * @param code the NUIT code to check
+ * @returns true if it is a valid BI code and false otherwise
+ */
+export default function isValid(code: number): boolean {
+  return true;
+}
